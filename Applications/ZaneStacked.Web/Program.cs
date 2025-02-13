c444ce875fd25ddc604a9ce7aea7aec324c242ae
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-02-13 11:28:44 +0100

    add SkillList and ProjectList components with services for data retrieval

--- Applications/ZaneStacked.Web/Program.cs
+++ Applications/ZaneStacked.Web/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ZaneStacked.Web.Components;
+using ZaneStacked.Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,5 +9,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("https://localhost:7083") });
+
+builder.Services.AddScoped<SkillService>();
+builder.Services.AddScoped<ProjectService>();
 
 await builder.Build().RunAsync();
