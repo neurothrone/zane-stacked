ecd24571488c685753c15c3af4396b6844463f47
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-02-19 15:06:53 +0100

    add focusElement function; implement functionality to focus on a specified element

--- Applications/ZaneStacked.Web/wwwroot/js/app.js
+++ Applications/ZaneStacked.Web/wwwroot/js/app.js
@@ -6,6 +6,12 @@ window.getWindowWidth = () => {
     return window.innerWidth;
 };
 
+window.focusElement = (element) => {
+    if (element) {
+        element.focus();
+    }
+};
+
 document.addEventListener("DOMContentLoaded", function () {
     let savedTheme = localStorage.getItem("theme") || "dark";
     setTheme(savedTheme);
