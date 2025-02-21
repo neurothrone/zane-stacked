6015e595251ad50abc04b2f3f7fcbe9af3959abc
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-02-21 20:16:51 +0100

    feat: add tooltip functionality to navigation rail for improved user guidance

--- Applications/ZaneStacked.Web/wwwroot/js/app.js
+++ Applications/ZaneStacked.Web/wwwroot/js/app.js
@@ -16,6 +16,18 @@ window.setAdminTheme = function () {
     document.documentElement.setAttribute("data-bs-theme", "dark");
 };
 
+window.enableNavRailTooltips = () => {
+    const navRail = document.querySelector('.navrail');
+    if (!navRail)
+        return;
+
+    const tooltipTriggerList = Array.from(navRail.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach(tooltipTriggerEl => {
+        new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+};
+
+
 document.addEventListener("DOMContentLoaded", function () {
     let savedTheme = localStorage.getItem("theme") || "dark";
     setTheme(savedTheme);
