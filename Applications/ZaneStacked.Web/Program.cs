33e9f51f66826075d83ee7f92a1cc708b96bd24b
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-02-27 00:39:40 +0100

    refactor: simplify HttpClient registration in Program.cs

--- Applications/ZaneStacked.Web/Program.cs
+++ Applications/ZaneStacked.Web/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddScoped(
 // {
 //     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 // });
-builder.Services.AddScoped(sp => new HttpClient
+builder.Services.AddScoped(_ => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
     DefaultRequestHeaders =
